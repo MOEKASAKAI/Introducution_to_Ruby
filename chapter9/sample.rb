86f1616a7492e6d9f7ed440efa0b40be5821b9aa
MOEKASAKAI <wpi.floves2@gmail.com>
2021-02-21 13:37:27 +0900

    [Add] chapter9 例外処理実行例

--- chapter9/sample.rb
+++ chapter9/sample.rb
@@ -4,6 +4,13 @@ module Greeter
     'hello'
   end
 end
-greeter = Greeter.new
-# 上の行で例外が発生するため、ここから下は表示されない
+
+# 例外処理を組み込んで例外に対処する
+begin
+  greeter = Greeter.new
+rescue
+  puts '例外が発生したが、このまま続行する'
+end
+
+# 例外処理を組み込んだので、最後まで実行可能
 puts 'End.'
